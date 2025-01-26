f3569fd8817b25a44ebd9e9b38b6851ff3b3b189
dmckay <dmckay@managewaresolutions.com>
2025-01-25 20:18:38 -0700

    AI - Co Pilot Generated Pages - Looks Decent for Proof of Concept

--- GenericWebApp.DTO/Management/Task.cs
+++ GenericWebApp.DTO/Management/Task.cs
@@ -4,14 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace GenericWebApp.DTO.Management
 {
     public class TaskItem
     {
         public int? ID { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(500, ErrorMessage = "Title cannot exceed 500 characters")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
+
         public string TaskObjectType_Code { get; set; }
         public int? Task_Object_ID { get; set; }
         public int? TaskActivity_ID { get; set; }
@@ -22,12 +29,12 @@ namespace GenericWebApp.DTO.Management
         {
             if (String.IsNullOrEmpty(Title))
             {
-                errorList.Add(new Error { Message = "Task Title Required" });
+                errorList.Add(new Error { Code="DTO.Invalid", Message = "Task Title Required" });
             }
 
             if (String.IsNullOrEmpty(Description))
             {
-                errorList.Add(new Error { Message = "Task Description Required" });
+                errorList.Add(new Error { Code= "DTO.Invalid", Message = "Task Description Required" });
             }
 
             return errorList.Count == 0;
